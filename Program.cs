2dfab244b22bc6452392a9b98c52ce1c866d90a9
ramtinbozorgi@web.de <ramtinbozorgi@web.de>
2020-05-18 21:59:09 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int n = int.Parse(text);
+            int factorial=1;
+            for(int i=1; i<=n; i++)
+            {
+                factorial *= i;
+            }
+            Console.WriteLine("Factorial of {0} is {1}", n, factorial);
+        }
+    }
+}
